9c34e52b736c62f26ee77ffb60a4b4e8b577bb6a
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-02-03 00:32:10 +0200

    target: add handling of state; add basic handling of logins

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"path"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/BurntSushi/toml"
 
@@ -15,6 +21,61 @@ import (
 
 type targetConfig struct {
 	BaseURL string
+}
+
+type targetState struct {
+	Collector *colly.Collector
+}
+
+type clientAuth struct {
+	ExchangeKey string
+	AuthKey     string
+	SignKey     string
+}
+
+type clientState struct {
+	Auth *clientAuth
+}
+
+type serverState struct {
+	ClientsMutex sync.RWMutex
+	Clients      map[string]*clientState
+}
+
+type responseBody struct {
+	Status int         `json:"status"`
+	Data   interface{} `json:"data"`
+}
+
+const nanosecsInMicrosec = 1000
+const microsecsInSec = 1000000
+
+var randomSeed string
+var randomSeedLastUpdate int64
+var wasSeeded bool
+
+func microtime() string {
+	var tv *syscall.Timeval
+	syscall.Gettimeofday(tv)
+	msec := tv.Usec / nanosecsInMicrosec
+	sec := tv.Sec
+	return fmt.Sprintf("%f %d", float64(msec)/microsecsInSec, sec)
+}
+
+func generateUniqueID(extra string) string {
+	uniqueID := randomSeed + microtime()
+	uniqueIDHashStr := fmt.Sprintf("%x", md5.Sum([]byte(uniqueID)))
+	newRandomSeed := fmt.Sprintf("%x", md5.Sum([]byte(randomSeed+uniqueIDHashStr+extra)))
+	if wasSeeded == false && randomSeedLastUpdate < time.Now().Unix()-int64(1+rand.Intn(11)) {
+		randomSeedLastUpdate = time.Now().Unix()
+		randomSeed = newRandomSeed
+		wasSeeded = true
+	}
+	return uniqueIDHashStr[4:20] // blaze it
+}
+
+func generateDefaultUniqueID() string {
+	return generateUniqueID("c")
 }
 
 func main() {
@@ -40,7 +101,61 @@ func main() {
 		return
 	}
 
-	collector := colly.NewCollector()
+	target := targetState{
+		Collector: colly.NewCollector(),
+	}
+
+	target.Collector.OnResponse(func(response *colly.Response) {
+		if response.Request.URL.Path == "/ucp.php" && response.Request.URL.Query().Get("mode") == "login" {
+			switch response.StatusCode {
+			case http.StatusOK:
+				log.Printf("successfully logged into %s\n", response.Request.URL.Host)
+
+			default:
+				log.Printf("failed to log into %s: target responded with HTTP status code %d\n", response.Request.URL.Host, response.StatusCode)
+			}
+		}
+	})
+
+	server := serverState{
+		Clients: make(map[string]*clientState),
+	}
+
+	http.HandleFunc("/auth/generate_keys", func(writer http.ResponseWriter, request *http.Request) {
+		log.Printf("authorization request initiated by %s\n", request.RemoteAddr)
+		log.Printf("generating authentication key triple for %s...\n", request.RemoteAddr)
+
+		client := clientState{
+			Auth: &clientAuth{
+				ExchangeKey: generateDefaultUniqueID(),
+				AuthKey:     generateDefaultUniqueID(),
+				SignKey:     generateDefaultUniqueID(),
+			},
+		}
+
+		server.ClientsMutex.Lock()
+		server.Clients[request.RemoteAddr] = &client
+		server.ClientsMutex.Unlock()
+
+		data := map[string]string{
+			"exchange_key": client.Auth.ExchangeKey,
+		}
+		response := responseBody{
+			Status: 200,
+			Data:   data,
+		}
+		responseText, err := json.Marshal(response)
+		if err != nil {
+			log.Printf("authorization request by %s failed: could not serialize response data to JSON: %s\n", request.RemoteAddr, err.Error())
+			return
+		}
+
+		_, err = writer.Write(responseText)
+		if err != nil {
+			log.Printf("authorization request by %s failed: could not send serialized response data: %s\n", request.RemoteAddr, err.Error())
+			return
+		}
+	})
 
 	http.HandleFunc("/x/auth/login", func(writer http.ResponseWriter, request *http.Request) {
 		log.Println("login request arrived from", request.RemoteAddr)
@@ -72,7 +187,7 @@ func main() {
 		username := usernames[0]
 		password := passwords[0]
 
-		err := collector.Post(config.BaseURL+"/ucp.php?mode=login", map[string]string{
+		err := target.Collector.Post(config.BaseURL+"/ucp.php?mode=login", map[string]string{
 			"username": username,
 			"password": password,
 			"login":    "Влез",
