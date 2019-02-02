c0c75ed7676d36c8626f48eecc1229ba64c40bd4
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-02-02 19:21:55 +0200

    implement crawler based on Colly

--- main.go
+++ main.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
-	c := colly.NewCollector()
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
+	collector := colly.NewCollector()
+	collector.OnHTML("a[href]", func(element *colly.HTMLElement) {
+		element.Request.Visit(element.Attr("href"))
 	})
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+	collector.OnRequest(func(request *colly.Request) {
+		fmt.Println("Visiting", request.URL)
 	})
-	c.Visit("http://go-colly.org/")
+
+	http.HandleFunc("/crawl", func(writer http.ResponseWriter, request *http.Request) {
+		request.ParseForm()
+		urls, ok := request.Form["url"]
+		if !ok || len(urls) < 1 {
+			log.Println("request error: no URL provided!")
+			return
+		}
+
+		for _, url := range urls {
+			log.Println("request arrived from", request.Host, "to crawl over the links in", url)
+			collector.Visit(url)
+		}
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
