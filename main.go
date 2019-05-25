58c05a13dd8210ecbe4be325d5075987dce5c4ce
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-05-25 16:28:17 +0300

    api: implement listing of forums, topics and posts

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/hex"
 	"encoding/json"
@@ -12,9 +13,12 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"regexp"
+	"strconv"
 	"sync"
 
 	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 
 	"github.com/BurntSushi/toml"
 	"github.com/gorilla/mux"
@@ -34,6 +38,9 @@ type serverState struct {
 	Clients map[string]*clientState // map from the username of the client to its correspoding clientState instance
 	AuthMap map[string]*clientState // map from the access token for the client to its correspoding clientState instance
 }
+
+var forumIDMatcher = regexp.MustCompile(`\./viewforum\.php\?f=(\d+)`)
+var topicIDMatcher = regexp.MustCompile(`\./viewtopic\.php\?f=\d+&t=(\d+)`)
 
 func newClient() (client *clientState, err error) {
 	cookieJar, err := cookiejar.New(nil)
@@ -179,6 +186,303 @@ func main() {
 		_, err = writer.Write(responseText)
 		if err != nil {
 			log.Printf("failed to log into `%s` from %s with username `%s`: could not send serialized HTTP response data: %s\n", configName, request.RemoteAddr, username, err.Error())
+			return
+		}
+	})
+
+	router.HandleFunc("/forums", func(writer http.ResponseWriter, request *http.Request) {
+		log.Printf("attempting to retrieve the list of forums at `%s` by `%s`...\n", configName, request.RemoteAddr)
+
+		accessToken := request.FormValue("access_token")
+		if accessToken == "" {
+			log.Printf("failed to retrieve the list of forums at `%s` by `%s`: no access token provided\n", configName, request.RemoteAddr)
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		server.Mutex.Lock()
+		client, isLoggedIn := server.AuthMap[accessToken]
+		server.Mutex.Unlock()
+		if !isLoggedIn {
+			log.Printf("failed to retrieve the list of forums at `%s` by `%s`: client with access token `%s` is not logged in\n", configName, request.RemoteAddr, accessToken)
+			return
+		}
+
+		serverResponse, err := client.Get(config.BaseURL)
+		if err != nil {
+			log.Printf("failed to retrieve the list of forums at `%s` by user with access token `%s`: could not initiate GET request: %s\n", configName, accessToken, err.Error())
+			return
+		}
+		defer serverResponse.Body.Close()
+
+		writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+		writer.WriteHeader(http.StatusOK)
+
+		document, err := html.Parse(serverResponse.Body)
+		if err != nil {
+			log.Printf("failed to retrieve the list of forums at `%s` by user `%s`: could not parse index HTML page: %s\n", configName, accessToken, err.Error())
+			return
+		}
+
+		forumLinks := scrape.FindAll(document, scrape.ByClass("forumlink"))
+		response := make([]map[string]interface{}, len(forumLinks))
+
+		for i, forumLink := range forumLinks {
+			forumMetadata := map[string]interface{}{}
+
+			var forumURL string
+			for _, attr := range forumLink.Attr {
+				if atom.Lookup([]byte(attr.Key)) == atom.Href {
+					forumURL = attr.Val
+				}
+			}
+			if forumURL == "" {
+				log.Printf("warning: found forum link with empty URL for forum with index %d; skipping...\n", i)
+				continue
+			}
+
+			forumIDMatches := forumIDMatcher.FindStringSubmatch(forumURL)
+			forumMetadata["id"] = forumIDMatches[1]
+
+			forumLinkText := forumLink.FirstChild
+			if forumLinkText == nil || forumLinkText.Type != html.TextNode {
+				log.Printf("warning: could not find name of forum with index %d\n", i)
+				continue
+			}
+			forumMetadata["name"] = forumLinkText.Data
+
+			response[i] = forumMetadata
+		}
+
+		forumDescriptions := scrape.FindAll(document, scrape.ByClass("forumdesc"))
+		for i := 0; i < len(response) && i < len(forumDescriptions); i++ {
+			forumDescriptionText := forumDescriptions[i].FirstChild
+			if forumDescriptionText == nil || forumDescriptionText.Type != html.TextNode {
+				log.Printf("warning: could not find description of forum with index %d\n", i)
+				continue
+			}
+			response[i]["description"] = forumDescriptionText.Data
+		}
+
+		responseText, err := json.Marshal(response)
+		if err != nil {
+			log.Printf("failed to retrieve the list of forums at `%s` by user with access token `%s`: could not serialize response data to JSON: %s\n", configName, accessToken, err.Error())
+			return
+		}
+
+		_, err = writer.Write(responseText)
+		if err != nil {
+			log.Printf("failed to retrieve the list of forums at `%s` by user with access token `%s`: could not send serialized response data: %s\n", configName, accessToken, err.Error())
+			return
+		}
+	})
+
+	router.HandleFunc("/topics/{forumID}", func(writer http.ResponseWriter, request *http.Request) {
+		log.Printf("attempting to retrieve list of topics at `%s` by `%s`...\n", configName, request.RemoteAddr)
+
+		accessToken := request.FormValue("access_token")
+		if accessToken == "" {
+			log.Printf("failed to retrieve list of topics at `%s` by `%s`: no access token provided\n", configName, request.RemoteAddr)
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		forumID, ok := mux.Vars(request)["forumID"]
+		if !ok {
+			log.Printf("failed to retrieve list of topics at `%s` by `%s`: no forum ID provided\n", configName, request.RemoteAddr)
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		server.Mutex.Lock()
+		client, isLoggedIn := server.AuthMap[accessToken]
+		server.Mutex.Unlock()
+		if !isLoggedIn {
+			log.Printf("failed to retrieve list of topics at `%s` by `%s`: client with access token `%s` is not logged in\n", configName, request.RemoteAddr, accessToken)
+			return
+		}
+
+		serverResponse, err := client.Get(config.BaseURL + "/viewforum.php?f=" + forumID)
+		if err != nil {
+			log.Printf("failed to retrieve list of topics at `%s` by user with access token `%s`: could not initiate GET request: %s\n", configName, accessToken, err.Error())
+			return
+		}
+		defer serverResponse.Body.Close()
+
+		writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+		writer.WriteHeader(http.StatusOK)
+
+		document, err := html.Parse(serverResponse.Body)
+		if err != nil {
+			log.Printf("failed to retrieve list of topics at `%s` by user `%s`: could not parse index HTML page: %s\n", configName, accessToken, err.Error())
+			return
+		}
+
+		topicLinks := scrape.FindAll(document, scrape.ByClass("topictitle"))
+		response := make([]map[string]interface{}, len(topicLinks))
+
+		for i, topicLink := range topicLinks {
+			topicMetadata := map[string]interface{}{}
+
+			var topicURL string
+			for _, attr := range topicLink.Attr {
+				if atom.Lookup([]byte(attr.Key)) == atom.Href {
+					topicURL = attr.Val
+				}
+			}
+			if topicURL == "" {
+				log.Printf("warning: found topic link with empty URL for topic with index %d; skipping...\n", i)
+				continue
+			}
+
+			topicIDMatches := topicIDMatcher.FindStringSubmatch(topicURL)
+			topicMetadata["id"] = topicIDMatches[1]
+
+			topicLinkText := topicLink.FirstChild
+			if topicLinkText == nil || topicLinkText.Type != html.TextNode {
+				log.Printf("warning: could not find name of topic with index %d\n", i)
+				continue
+			}
+			topicMetadata["name"] = topicLinkText.Data
+
+			response[i] = topicMetadata
+		}
+
+		responseText, err := json.Marshal(response)
+		if err != nil {
+			log.Printf("failed to retrieve the list of forums at `%s` by user with access token `%s`: could not serialize response data to JSON: %s\n", configName, accessToken, err.Error())
+			return
+		}
+
+		_, err = writer.Write(responseText)
+		if err != nil {
+			log.Printf("failed to retrieve the list of forums at `%s` by user with access token `%s`: could not send serialized response data: %s\n", configName, accessToken, err.Error())
+			return
+		}
+	})
+
+	router.HandleFunc("/posts/{forumID}/{topicID}/{pageNumber}", func(writer http.ResponseWriter, request *http.Request) {
+		log.Printf("attempting to retrieve list of posts at `%s` by `%s`...\n", configName, request.RemoteAddr)
+
+		accessToken := request.FormValue("access_token")
+		if accessToken == "" {
+			log.Printf("failed to retrieve list of posts at `%s` by `%s`: no access token provided\n", configName, request.RemoteAddr)
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		vars := mux.Vars(request)
+
+		forumID, ok := vars["forumID"]
+		if !ok {
+			log.Printf("failed to retrieve list of posts at `%s` by `%s`: no forum ID provided\n", configName, request.RemoteAddr)
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		topicID, ok := vars["topicID"]
+		if !ok {
+			log.Printf("failed to retrieve list of posts at `%s` by `%s`: no topic ID provided\n", configName, request.RemoteAddr)
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		pageNumberStr, ok := vars["pageNumber"]
+		if !ok {
+			log.Printf("failed to retrieve list of posts at `%s` by `%s`: no page number provided\n", configName, request.RemoteAddr)
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		pageNumber, err := strconv.Atoi(pageNumberStr)
+		if err != nil {
+			log.Printf("failed to retrieve list of posts at `%s` by `%s`: could not parse page number\n", configName, request.RemoteAddr)
+			return
+		}
+		if pageNumber < 1 {
+			log.Printf("failed to retrieve list of posts at `%s` by `%s`: page number must be a positive integer\n", configName, request.RemoteAddr)
+			return
+		}
+
+		server.Mutex.Lock()
+		client, isLoggedIn := server.AuthMap[accessToken]
+		server.Mutex.Unlock()
+		if !isLoggedIn {
+			log.Printf("failed to retrieve list of posts at `%s` by `%s`: client with access token `%s` is not logged in\n", configName, request.RemoteAddr, accessToken)
+			return
+		}
+
+		serverResponse, err := client.Get(config.BaseURL + "/viewtopic.php?f=" + forumID + "&t=" + topicID + "&start=" + strconv.Itoa(15*(pageNumber-1)))
+		if err != nil {
+			log.Printf("failed to retrieve list of posts at `%s` by user with access token `%s`: could not initiate GET request: %s\n", configName, accessToken, err.Error())
+			return
+		}
+		defer serverResponse.Body.Close()
+
+		writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+		writer.WriteHeader(http.StatusOK)
+
+		document, err := html.Parse(serverResponse.Body)
+		if err != nil {
+			log.Printf("failed to retrieve list of posts at `%s` by user `%s`: could not parse index HTML page: %s\n", configName, accessToken, err.Error())
+			return
+		}
+
+		postBodies := scrape.FindAll(document, scrape.ByClass("postbody"))
+		response := make([]map[string]interface{}, 0, len(postBodies))
+
+		for i, postBody := range postBodies {
+			if postBody.DataAtom != atom.Div {
+				continue
+			}
+
+			post := map[string]interface{}{}
+
+			var postTextBuffer bytes.Buffer
+			for postBodyChildElement := postBody.FirstChild; postBodyChildElement != nil; postBodyChildElement = postBodyChildElement.NextSibling {
+				err := html.Render(&postTextBuffer, postBodyChildElement)
+				if err != nil {
+					log.Printf("warning: could not render body of post with index %d\n", i)
+					continue
+				}
+			}
+			post["body"] = postTextBuffer.String()
+
+			response = append(response, post)
+		}
+
+		postSignatures := scrape.FindAll(document, scrape.ByClass("signature"))
+		for i := 0; i < len(response) && i < len(postSignatures); i++ {
+			var postSignatureTextBuffer bytes.Buffer
+			postSignature := postSignatures[i]
+			for postSignatureChildElement := postSignature.FirstChild; postSignatureChildElement != nil; postSignatureChildElement = postSignatureChildElement.NextSibling {
+				err := html.Render(&postSignatureTextBuffer, postSignatureChildElement)
+				if err != nil {
+					log.Printf("warning: could not render signature of post with index %d\n", i)
+					continue
+				}
+			}
+			response[i]["signature"] = postSignatureTextBuffer.String()
+		}
+
+		postAuthors := scrape.FindAll(document, scrape.ByClass("postauthor"))
+		for i := 0; i < len(response) && i < len(postAuthors); i++ {
+			postAuthorName := postAuthors[i].FirstChild
+			if postAuthorName == nil || postAuthorName.Type != html.TextNode {
+				log.Printf("warning: could not find author of post with index %d\n", i)
+				continue
+			}
+			response[i]["author"] = postAuthorName.Data
+		}
+
+		responseText, err := json.Marshal(response)
+		if err != nil {
+			log.Printf("failed to retrieve the list of forums at `%s` by user with access token `%s`: could not serialize response data to JSON: %s\n", configName, accessToken, err.Error())
+			return
+		}
+
+		_, err = writer.Write(responseText)
+		if err != nil {
+			log.Printf("failed to retrieve the list of forums at `%s` by user with access token `%s`: could not send serialized response data: %s\n", configName, accessToken, err.Error())
 			return
 		}
 	})
